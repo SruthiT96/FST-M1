e18c92237638ae2982ff8742a6c0a7c626a11bf4
SruthiT96 <Sruthi.tirumalaraju@ibm.com>
2026-08-06 16:43:47 +0530

    Update Activity1.py

--- Python/Activities/Activity1.py
+++ Python/Activities/Activity1.py
@@ -1,6 +1,6 @@
 name=input("What is your name?")
 print(" "+name)
-age=input("What is your age?")
+age=input("How old are you?")
 print(" "+age)
 ageHundred=str(2026-int(age)+100);
 print(name+" will turn 100 years in the year "+ageHundred)
