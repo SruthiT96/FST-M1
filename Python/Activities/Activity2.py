0688844d71761a894f05600e1359f0583b273050
SruthiT96 <Sruthi.tirumalaraju@ibm.com>
2026-08-06 16:44:52 +0530

    Fix output messages for odd/even number check

--- Python/Activities/Activity2.py
+++ Python/Activities/Activity2.py
@@ -1,6 +1,6 @@
 num=input("Enter a number")
 print(num)
 if int(num)%2==0:
-    print("Given number is an even number")
+    print("You picked an odd number")
 else:
-    print("Given number is an odd number")
+    print("You picked an even number")
